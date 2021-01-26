c9c20d680cdb1f40a7e961f3168ffc1664dc65ec
Jaebeen <sks11241124@gmail.com>
2021-01-26 11:47:15 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main(){
     for(int i=L;i<=100;i++){
       int temp = N-i*(i+1)/2;
 
-      if(temp%i==0&&temp/i+1>=0){
-          for(int j=temp/i+1;j<=temp/i+i;j++){
+      if(temp%i==0&&temp/i+1>=0){//해당 문제에서 temp/i+1<0인 경우가 test case에 있어서 이를 방지
+          for(int j=temp/i+1;j<=temp/i+i;j++){//확정된 L은 i 이다.
             cout<<j<<" ";
           }
     
